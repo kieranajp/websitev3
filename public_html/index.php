0e7f170b498a42895b83128fda2364ce94190c6d
Kieran Patel <me@kieranajp.co.uk>
2015-12-06 12:27:15 +0000

    Tweaks for responsiveness

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>JSPM Test</title>
+    <title>Kieran Patel | Web Developer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="fonts.css">
     <link rel="stylesheet" href="reset.css">
 </head>
